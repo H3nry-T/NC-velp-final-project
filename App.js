1b0fcdb80bff2492cd8c01fc12e6a42e5f9c028a
H3nry-T <ht3drex@gmail.com>
2023-02-19 15:52:18 +0000

    created a navbar and tailwind added

--- App.js
+++ App.js
@@ -4,21 +4,39 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
+import NavBar from "./components/NavBar";
+import { auth } from "./firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
 
 const Stack = createNativeStackNavigator();
+
 export default function App() {
+  const [authenticated, setAuthenticated] = useState(false);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setAuthenticated(!!user);
+    });
+    //event listener returns a clean up function
+    return unsubscribe;
+  }, []);
+
+  // I want a navbar to appear here if the user is logged in!
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          options={{ headerShown: false }}
-          name="Login"
-          component={LoginScreen}
-        />
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Map" component={Map} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      {authenticated === true ? <NavBar /> : ""}
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Login"
+            component={LoginScreen}
+          />
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Map" component={Map} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
---
+++ components/NavBar.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <View className="flex flex-1 justify-center items-center">
+      <Text className="bg-slate-600">NavBar</Text>
+    </View>
+  );
+};
+
+export default NavBar;
+
+const styles = StyleSheet.create({});
