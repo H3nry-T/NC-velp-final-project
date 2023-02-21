058c6efabbd8e9ad0049a4683e07bf24f70e6921
H3nry-T <ht3drex@gmail.com>
2023-02-21 10:52:15 +0000

    signoutButton colored

--- components/SignoutButton.jsx
+++ components/SignoutButton.jsx
@@ -4,8 +4,11 @@ import { handleSignOut } from "../firebase/auth/signout";
 
 export function SignoutButton({ replace }) {
   return (
-    <TouchableOpacity onPress={() => handleSignOut(replace)}>
-      <Text>Sign Out</Text>
+    <TouchableOpacity
+      onPress={() => handleSignOut(replace)}
+      className=" rounded-full bg-sky-500 p-2 w-2/5 my-4"
+    >
+      <Text className="font-bold text-center text-white">Sign Out</Text>
     </TouchableOpacity>
   );
 }
