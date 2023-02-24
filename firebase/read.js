f5a8882af20ed9c12e362ca812d60c410354f2c8
AleaToir3 <aleatoir3@gmail.com>
2023-02-24 17:06:17 +0000

    we have Userid

--- firebase/read.js
+++ firebase/read.js
@@ -1,41 +1,23 @@
-// // import axios from "axios";
-// import { getFirestore, collection, query, where, getDocs  } from "firebase/firestore";
+// import axios from "axios";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
 
-// const dataBase = getFirestore();
-// const testEventsCollection = collection(dataBase, "test_events");
-// export const getTestEvents = async () => {
-//   try {
-//     const myQueryFireBase = query(testEventsCollection, where("id", "==", "WPBPeJ7P30cPeqEOY4RO"));
-//     const snapshot = await getDocs(myQueryFireBase);
+const dataBase = getFirestore();
+const testEventsCollection = collection(dataBase, "test_events");
 
-//     const testEvents = [];
-//     const result = snapshot.docs[0].data()
-//     console.log("🔥result:", result);
+export const getTestEvents = async () => {
+  try {
+    const snapshot = await getDocs(testEventsCollection);
 
-    
-//   } catch (error) {
-//     console.log(error); 
-//   }
+    const testEvents = [];
+    snapshot.docs.forEach((doc) => {
+      const tempData = doc.data();
+      testEvents.push(tempData);
+    });
+    // console.log(testEvents);
+    return testEvents;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-// }
 
-
-
-import { getFirestore, collection, query, where, getDocs, addDoc ,updateDoc} from "firebase/firestore";
-
-const db = getFirestore();
-const myDataFromFireBase = collection(db, "test_events");
-
-// add data on firebase
-// export const createEvents = async () => {
-//   await addDoc(myDataFromFireBase, {name:"JASOONqqq",event:"qqqEVENT1"});
-//  console.log("DATA SEND TO FIREBASE");
-// };
-const updateData = async () => {
-  const id = "zbj87Ysm5i5vZA7GEFzb"
-  const mAj = {email:"test123@yoyo.com"};
-  const myEven = doc(db, "test_events", id)
-  console.log("🚨🔥:", myEven);
-
-  // await updateDoc(myDataFromFireBase, mAj);
-}
--- firebase/update.js
+++ firebase/update.js
@@ -1,9 +1,41 @@
 
 //UPDATE DATA WHEN USER ADD HIMSELF TO THE EVENTS
+import { getAuth } from "firebase/auth";
 
 
+// ---------------
 
-//STEP CHANGE DATA "ANY"
+//1 STEP CHANGE DATA "ANY"
+const myupdate = async () => {
+    try {
+      const ref = doc(db, "test_events", "zbj87Ysm5i5vZA7GEFzb");
+      await updateDoc(ref, {allo: "update@update.com"});
+      console.log("Document updated successfully");
+    } catch (e) {
+      console.error("Error updating document:", e);
+    }
+  }
+  
+  myupdate();
+const [user, setUser] = useState(null);
+
+useEffect(() => {
+  const unsubscribe = auth.onAuthStateChanged((user) => {
+    setUser(user);
+    console.log("🚨🔥  file: update.js:25  unsubscribe  user:", user);
+  });
+
+
+  return unsubscribe;
+}, []);
 //GET USERNAME FROM USER WHEN IS LOGED IN
 
+const auth = getAuth();
+const authUser = auth.currentUser;
+console.log("🚨🔥  file: update.js:25  unsubscribe  user:", authUser);
+
 //WE ADD USER IN THE EVENT
+
+
+
+//increment the number of people in the event
