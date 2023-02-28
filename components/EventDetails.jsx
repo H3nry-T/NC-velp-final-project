2f8fd2d40852bcedee85f80df0d9256e958111ad
thebonottox <theb.shopping@gmail.com>
2023-02-28 15:12:18 +0000

    Fixed dates format on list of events and evdetails card

--- components/EventDetails.jsx
+++ components/EventDetails.jsx
@@ -1,11 +1,11 @@
 import { Text, TouchableOpacity, View, Modal, Pressable } from "react-native";
 import { Entypo } from "@expo/vector-icons";
-import {registerOnEvent} from "../firebase/update"
+import { registerOnEvent } from "../firebase/update";
+import { formatTimeStamp } from "./EventCard";
 
 export function EventDetails({ event, onClose }) {
-
   return (
-    <Modal animationType="slide" visible={true} >
+    <Modal animationType="slide" visible={true}>
       <View className=" bg-sky-200 flex-1 justify-center items-center">
         <View className="w-11/12 min-h-96 bg-white rounded-md shadow-lg">
           <TouchableOpacity onPress={onClose}>
@@ -14,16 +14,15 @@ export function EventDetails({ event, onClose }) {
           <View className="p-3">
             <Text className="text-2xl font-bold ml-3">{event.event_name}</Text>
             <Text className="pt-4 ml-3">{event.description}</Text>
-            <Text className="pt-4 ml-3">
-              When: {event.date_time.toDate().toLocaleString()}
-            </Text>
+            <Text className="pt-4 ml-3">When: {formatTimeStamp(event)}</Text>
             <Text className="pt-4 ml-3">Location: {event.address}</Text>
             <Text className="pt-4 ml-3">
               Organised by: {event.organisation_name.substring(0, 45)}
             </Text>
             <View className="flex-row items-center">
-              <TouchableOpacity className="flex-row justify-center w-7/12 mt-10 ml-5 bg-sky-200 p-4 rounded-full"
-              onPress={()=>registerOnEvent(event)}
+              <TouchableOpacity
+                className="flex-row justify-center w-7/12 mt-10 ml-5 bg-sky-200 p-4 rounded-full"
+                onPress={() => registerOnEvent(event)}
               >
                 <Text className="text-lg">I want to help!</Text>
               </TouchableOpacity>
