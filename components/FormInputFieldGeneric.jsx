3b74a26d00b81e9e7a6380318226dd3586d9313b
code-emad <emadul.islam@hotmail.co.uk>
2023-03-01 14:22:18 +0000

    add event page styled

--- components/FormInputFieldGeneric.jsx
+++ components/FormInputFieldGeneric.jsx
@@ -4,9 +4,9 @@ import React from "react";
 const FormInputFieldGeneric = ({ label, formDataField, onChange }) => {
   return (
     <View className="my-2 ">
-      <Text className="ml-6 mb-1">{label}</Text>
+      <Text className="ml-6 mb-1 font-bold">{label}</Text>
       <TextInput
-        className="bg-white rounded-full w-11/12 mx-auto px-2"
+        className="bg-white rounded-full w-11/12 mx-auto px-2 border-dotted border border-cyan-800"
         value={formDataField}
         onChangeText={(text) => {
           onChange(text);
